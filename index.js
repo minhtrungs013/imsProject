36e66cd13594c9ff0115e7093c0a232c84f7c562
lengochuy140799 <lengochuy789@gmail.com>
2022-04-04 15:58:55 +0700

    update

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ dotenv.config();
 const port = process.env.PORT;
 const cors = require("cors");
 app.use(cors());
+const { requireToken } = require("./middleware/index");
 const authRoute = require("./routers/auth");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", authRoute);
 const mentor = require("./routers/mentor");
-app.use("/mentor", mentor);
+app.use("/mentor", requireToken, mentor);
 app.listen(port, () => {
   console.log("App start success");
 });
