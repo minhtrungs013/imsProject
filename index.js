b36f6b0f5ee7433997bd2c2f0bd9cd3e4a9cedb5
dung dieptuy <dungdt1511@gmail.com>
2022-04-06 10:22:04 +0700

    swagger

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 dotenv.config();
 const port = process.env.PORT;
 // fix bug error CORS
 const cors = require("cors");
 app.use(cors());
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Express API for JSONPlaceholder",
+    version: "1.0.0",
+    description:
+      "This is a REST API application made with Express. It retrieves data from JSONPlaceholder.",
+    license: {
+      name: "Licensed Under MIT",
+      url: "https://spdx.org/licenses/MIT.html",
+    },
+    contact: {
+      name: "JSONPlaceholder",
+      url: "https://jsonplaceholder.typicode.com",
+    },
+  },
+  servers: [
+    {
+      url: "http://localhost:5000",
+      description: "Development server",
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ["./routers/*.js"],
+};
 // import Router
+const specs = swaggerJsDoc(options);
 const authRoute = require("./routers/auth");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //ROUTES
-app.use("/", authRoute);
+app.use(authRoute);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.listen(port, () => {
   console.log("App start success");
--- routers/auth.js
+++ routers/auth.js
@@ -1,7 +1,40 @@
 const router = require("express").Router();
-const {login, profile} = require("../controllers/authController");
-const {requireToken} = require("../middleware/index")
+const { login, profile } = require("../controllers/authController");
+const { requireToken } = require("../middleware/index");
 
+/**
+ * @swagger
+ * /api/auth:
+ *   post:
+ *     summary: Login.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: The user's name.
+ *                 example: minhtrung1
+ *               password:
+ *                 type: string
+ *                 description: user's password.
+ *                 example: 12345
+ *     responses:
+ *       201:
+ *         description: Token.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                       type: string
+ *                       description: user's token.
+ *                       example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6Im1pbmh0cnVuZzEiLCJwYXNzd29yZCI6IjEyMzQ1IiwiaWF0IjoxNjQ5MjEyMjgwLCJleHAiOjE2NDk4MTcwODB9.sW4ZRIU_DuOnd7NVhFlRsdTu7Q7rieIwYlx9jsQx1ms
+ */
 router.post("/auth/login", login);
 router.get("/auth/profile", requireToken, profile);
 
