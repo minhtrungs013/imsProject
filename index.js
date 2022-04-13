33cbf5a94f2b4b8d2ebef993bbeb9f0a34ae3e5a
dung dieptuy <dungdt1511@gmail.com>
2022-04-13 10:12:34 +0700

    CRUD DG

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
 app.use(cors());
-const {requireToken} = require("./middleware/index")
+const { requireToken } = require("./middleware/index");
 
 // import Router
 const authRoute = require("./routers/auth");
 const courseRoute = require("./routers/course");
 const mentor = require("./routers/mentor");
-
+const dg = require("./routers/dg");
 
 dotenv.config();
 const port = process.env.PORT;
@@ -18,11 +18,11 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 //ROUTES
 app.use(authRoute);
-app.use(requireToken, courseRoute);
-app.use(requireToken, mentor);
+// app.use(requireToken, courseRoute);
+// app.use(requireToken, mentor);
+app.use(dg);
 
 app.listen(port, () => {
   console.log("App start success");
