5787154e189d59c40bf785994563453af9f0ad65
dung dieptuy <dungdt1511@gmail.com>
2022-04-13 11:17:01 +0700

    crud

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
 app.use(cors());
-const {requireToken} = require("./middleware/index")
+const { requireToken } = require("./middleware/index");
 
 // import Router
 const authRoute = require("./routers/auth");
 const courseRoute = require("./routers/course");
 const mentor = require("./routers/mentor");
-
+const dg = require("./routers/dg");
 
 dotenv.config();
 const port = process.env.PORT;
@@ -18,9 +18,9 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 //ROUTES
 app.use(authRoute);
+app.use(dg);
 app.use(requireToken, courseRoute);
 app.use(requireToken, mentor);
 
