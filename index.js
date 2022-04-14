7e9b3b02ab38f0574ad03b5c1f7fc2dbb816acee
lengochuy140799 <lengochuy789@gmail.com>
2022-04-14 13:52:17 +0700

    import_Candidate

--- index.js
+++ index.js
@@ -2,28 +2,29 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
+
 app.use(cors());
-const {requireToken} = require("./middleware/index")
+const { requireToken } = require("./middleware/index");
 
 // import Router
 const authRoute = require("./routers/auth");
 const courseRoute = require("./routers/course");
 const mentor = require("./routers/mentor");
-
+const importCandidate = require("./routers/import");
 
 dotenv.config();
 const port = process.env.PORT;
+global.__basedir = __dirname + "/..";
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 //ROUTES
 app.use(authRoute);
-app.use(requireToken, courseRoute);
-app.use(requireToken, mentor);
-
+app.use(courseRoute);
+app.use(mentor);
+app.use(importCandidate);
 app.listen(port, () => {
   console.log("App start success");
 });
---
+++ models/importCandidate.js
@@ -0,0 +1,201 @@
+module.exports = (sequelize, Sequelize) => {
+  const Candidate = sequelize.define("candidates", {
+    idCandidate: {
+      type: Sequelize.INTEGER,
+      length: 15,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    fullName: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    tel: { type: "string", length: 10 },
+    emailCandidate: { type: "string", validate: { isEmail: true } },
+    idDG: {
+      type: Sequelize.INTEGER,
+      length: 15,
+      foreignKey: {
+        name: "pk_candidate_dg",
+        table: "dg",
+        rules: {
+          onDelete: "CASCADE",
+          onUpdate: "RESTRICT",
+        },
+        mapping: {
+          idDG: "idDG",
+        },
+      },
+    },
+    interviewTime: { type: "time" },
+    interviewDate: { type: "date" },
+    status: "string",
+    remark: "string",
+    idMentor: {
+      type: Sequelize.INTEGER,
+      length: 15,
+      foreignKey: {
+        name: "pk_candidate_mentor",
+        table: "mentor",
+        rules: {
+          onDelete: "CASCADE",
+          onUpdate: "RESTRICT",
+        },
+        mapping: {
+          idMentor: "idMentor",
+        },
+      },
+    },
+    technicalComments: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    technicalScore: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    attitude: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    englishCommunication: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    comments: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    remarks: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    internshipDomain: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    preferredSkills: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    university: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    faculty: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    currentYearofStudy: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    studentID: { type: "string" },
+    preferredInternshipStartDate: { type: "date" },
+    preferredInternshipDuration: { type: "string" },
+    internshipSchedule: { type: "string" },
+    GPA: { type: "float", length: 15 },
+    idInternshipCourse: {
+      type: Sequelize.INTEGER,
+      length: 15,
+      foreignKey: {
+        name: "pk_candidate_internshipcouse",
+        table: "internshipcourse",
+        rules: {
+          onDelete: "CASCADE",
+          onUpdate: "RESTRICT",
+        },
+        mapping: {
+          idInternshipCourse: "idInternshipCourse",
+        },
+      },
+    },
+    graduationYear: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    projectExperience: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    expectedGraduationSchedule: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    remainingSubjects: { type: "string" },
+    covidVaccinationiInformation: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    certificationDate: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    covidVaccinationCertificate: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    interviewLink: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    interviewer: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    emailInterviewer: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    pcType: {
+      type: "string",
+      validate: {
+        len: [5, 255],
+      },
+    },
+    deleteAt: { type: Sequelize.TIME },
+  });
+  return Candidate;
+};
