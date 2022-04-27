2e26dfcb7be2b691388b1d3718871774e7661b5a
dung dieptuy <dungdt1511@gmail.com>
2022-04-27 09:20:27 +0700

    edit nad fix bug crud dg

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ const cors = require("cors");
 app.use(cors());
 const { requireToken } = require("./middleware/index");
 
-// import Router
 const authRoute = require("./routers/auth");
 const courseRoute = require("./routers/course");
 const candidate = require("./routers/candidate");
 const email = require("./routers/email");
 const mentor = require("./routers/mentor");
+const dg = require("./routers/dg");
 dotenv.config();
 const port = process.env.PORT;
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(dg);
 
-app.use(requireToken,email);
+app.use(requireToken, email);
 
 app.listen(port, () => {
   console.log("App start success");
