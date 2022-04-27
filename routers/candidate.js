2115a959df769363d7594a857c96746d85257ff6
PhucPC-01 <nguyenhoangphuc12a10@gmail.com>
2022-04-27 16:37:06 +0700

    update candidate

--- routers/candidate.js
+++ routers/candidate.js
@@ -3,4 +3,12 @@ const router = express.Router();
 const candidateController = require("../controllers/candidateController");
 router.put("/candidate/interview/:id", candidateController.update);
 
+router.get("/candidate/:id", candidateController.getCandidate);
+router.get("/candidate/batch/:id", candidateController.getBatch);
+
+router.put("/candidate/:id", candidateController.updateCandidate);
+router.delete("/candidate/:id", candidateController.remove);
+router.post("/candidate/create", candidateController.create);
+
+
 module.exports = router;
